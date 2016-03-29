cfc90752d99925b92dace292f5190c8a4fc9e769
Clézio Santos <cleziolima@gmail.com>
2016-03-29 12:45:30 -0300

    Adicionada validação para a rotina de edição do usuário

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -30,12 +30,13 @@ public class Users extends BaseController{
         User userDTO = new GsonBuilder().create().fromJson(request.params.get("body"), User.class);
         User user = User.findById(userDTO.getId());
         user.setName(userDTO.getName());
+        validation.valid(user);
         if (validation.hasErrors()) {
             error(HTTP_UNPROCESSABLE_ENTITY, new Gson().toJson(getListErrors(validation.errorsMap())));
         }
         user.save();
         renderJSON(user);
-    }
+     }
 
     public static void list(){
         List<User> users = User.findAll();
